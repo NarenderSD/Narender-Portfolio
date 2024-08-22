ed324c13e77778678ae4f7943c9b6eaa90094cf1
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-08-22 19:17:37 +0530

    Project U1

--- assets/js/script.js
+++ assets/js/script.js
@@ -109,7 +109,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["software development", "frontend development", "backend development", "web designing", "web development", "flutter development", "android development","frappe/erpnext development", "wordpress development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
